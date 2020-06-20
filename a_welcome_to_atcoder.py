51c127c38a0c30bbf1579047fb456b0a8e435fe2
NozomiTakiguchi <takiguchi19960122@gmail.com>
2020-06-20 18:10:53 +0900

    welcome to atcoder

---
+++ a_welcome_to_atcoder.py
@@ -0,0 +1,20 @@
+
+from decorator import trace_perf
+
+@trace_perf
+def run(a, b, c, s):
+    '''
+    高橋君はデータの加工が行いたいです。
+    整数 a, b, c と、文字列 s が与えられます。 
+    a + b + c
+    の計算結果と、文字列 
+    s
+    を並べて表示しなさい。
+    '''
+    print('{} {}'.format(a + b + c, s))
+
+if __name__ == '__main__':
+    a = int(input())
+    b, c = list(map(int, input().split()))
+    s = input()
+    run(a, b, c, s)
