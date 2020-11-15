ed3225ee57f31b7777aa76adcdb25b7d2d5af4f6
Nozomi Takiguchi <takisoccer@gmail.com>
2020-11-15 14:45:35 +0000

    practice d @abc 20201115

--- src/20201115/20201115_d.py
+++ src/20201115/20201115_d.py
@@ -5,30 +5,37 @@ def run(n, k, schedule):
     いもす法というのを使って爆速で計算できるらしい
 
     '''
-    # 一番遅くまで使う予定がある人を先頭にした
-    concurrent_users = [0] * 2*10**5
+    concurrent = [0] * (2*10**5 + 10)
     for i in range(n):
         start_time = schedule[i][0]
         end_time = schedule[i][1]
         amount = schedule[i][2]
-        amount_list = [amount] * (end_time - start_time)
 
-        for _i in range(start_time, end_time):
-            concurrent_users[_i] += amount
-            if concurrent_users[_i] > k:
-                print('No')
-                exit()
+        # # これのせいで O(NT) だけの時間がかかってしまう
+        # for _i in range(start_time, end_time):
+        #     concurrent[_i] += amount
+        #     if concurrent[_i] > k:
+        #         print('No')
+        #         exit()
+        
+        # 時刻 S になったら水の使用量が amount だけ増える
+        concurrent[start_time] += amount
+        # これは思いつかん (時刻 T になったら水の使用量が amount だけ「減る」)
+        concurrent[end_time] -= amount
+    
+    for i in range(len(concurrent) - 1):
+        # ↑ ですごく時間がかかっていたループをやめて、累積和を利用する（これがいもす法？）
+        concurrent[i+1] += concurrent[i]
+        if concurrent[i] > k:
+            print("No")
+            exit()
+
     print('Yes')
     
-    # if len([x for x in concurrent_users if x > k]) > 0:
+    # if len([x for x in concurrent if x > k]) > 0:
     #     print('No')
     # else:
     #     print('Yes')
-
-
-
-
-
 
 if __name__ == '__main__':
     n, w = map(int, input().split())
